6fdf0f891268c0845dd93c950c5c761332b75de0
qingaoay <qingaoya123@163.com>
2020-05-04 19:12:24 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-<<<<<<< HEAD
-    <HelloWorld msg="Welcome to Your Vue.js App 1122哈哈哈哈"/>
-=======
-    <HelloWorld msg="Welcome to Your Vue.js App 哈啛22方法恩恩重中之重饿法付喳哈"/>
->>>>>>> 2ba63adec69cf0534f8c73c139381bf56608ac87
+    <HelloWorld msg="Welcome to Your Vue.js App 11wwwwww222266ppp22哈哈哈哈"/>
   </div>
 </template>
 
